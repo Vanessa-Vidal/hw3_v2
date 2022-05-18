078a9f9aa6ea1e8b9a6f791dcf2c84d1d4da2d53
Vanessa-Vidal <vanessa.vidal@kellogg.northwestern.edu>
2022-05-18 17:00:07 +0000

    version2

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,23 @@
 class PostsController < ApplicationController
+
+    def index
+        @posts = Place.all
+      end
+
+    def new
+        @post= Place.new
+    end
+
+    def create
+        @post=Place.new
+        @post["title"]=params["post"]["title"]
+        @post["description"]=params["post"]["description"]
+        @post["date"]=params["post"]["posted_on"]
+        @posts.save
+        redirect_to "/places"
+    end
+
+
+
+
 end
